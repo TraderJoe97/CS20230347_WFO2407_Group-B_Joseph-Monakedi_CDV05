7c81eacd4e434c6b0b9894df3f5354de5fbb4009
Joseph Monakedi <99967046+TraderJoe97@users.noreply.github.com>
2024-11-06 15:11:20 +0200

    fix theme toggle formobile devices

--- script.js
+++ script.js
@@ -125,4 +125,38 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     });
+    // Theme switcher mobile
+    const themeToggleBtnMobile = document.getElementById('theme-toggle-mobile');
+    const themeToggleDarkIconMobile = document.getElementById('theme-toggle-dark-icon-mobile');
+    const themeToggleLightIconMobile = document.getElementById('theme-toggle-light-icon-mobile');
+
+    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+        themeToggleLightIconMobile.classList.remove('hidden');
+    } else {
+        themeToggleDarkIconMobile.classList.remove('hidden');
+    }
+
+    themeToggleBtnMobile.addEventListener('click', function() {
+        themeToggleDarkIconMobile.classList.toggle('hidden');
+        themeToggleLightIconMobile.classList.toggle('hidden');
+
+        if (localStorage.getItem('color-theme')) {
+            if (localStorage.getItem('color-theme') === 'light') {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            }
+        } else {
+            if (document.documentElement.classList.contains('dark')) {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            } else {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            }
+        }
+    });
 })
